46505f0bb6bbc1d73c31096b5050313c99cba515
Glitch (carona) <none>
2020-03-18 16:48:37 +0000

    💆🚖 Checkpoint ./style.css:12942465/62 ./script.js:12720927/229

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ var url = "https://api.kawalcorona.com"
 
 function getDetail(hasil){
   // console.log(data)
-  hasil.forEach(response=>{
-      let dataa = response.attributes
-      // console.log(dataa)
-      res.negara = dataa.Country_Region
-      res.lat = dataa.Lat
-      res.long = dataa.Long_
-      res.terinfeksi = dataa.Confirmed
-      res.meninggal = dataa.Deaths
-      res.sembuh = dataa.Recovered
-      res.aktif = dataa.Active
-      // res.last_update = new Date(1584469982000/1000)
-      // console.log(res.last_update)
-      // console.log(res.negara)
-      $("#negara").append(`<a class="dropdown-item" href="#">${res.negara}</a>`)  
-      $(".row").append(`<div class="col-lg-6 pb-5">`+
-          `<div class="card text-center">`+
+    hasil.forEach(response=>{
+        let dataa = response.attributes
+        // console.log(dataa)
+        res.negara = dataa.Country_Region
+        res.lat = dataa.Lat
+        res.long = dataa.Long_
+        res.terinfeksi = dataa.Confirmed
+        res.meninggal = dataa.Deaths
+        res.sembuh = dataa.Recovered
+        res.aktif = dataa.Active
+        // res.last_update = new Date(1584469982000/1000)
+        // console.log(res.last_update)
+        // console.log(res.negara)
+        $("#negara").append(`<a class="dropdown-item" href="#">${res.negara}</a>`)
+        $(".row").append(`<div class="col-lg-6 pb-5">`+
+            `<div class="card text-center">`+
             `<div class="card-header" id="nama-negara">`+
             `<h5 class="card-title">${res.negara}</h5>`+
             `</div>`+
@@ -33,8 +33,8 @@ function getDetail(hasil){
             `<p class="card-text">Total Aktif : ${res.aktif}</p>`+
             // `<p class="card-text">Terakhir Update : ${res.last_update}</p>`+
             `</div>`+
-          `</div>`+
-       `</div>`)
+            `</div>`+
+        `</div>`)
     // $("#hasil").append(`<p class="card-text">Total Terinfeksi : ${res.terinfeksi}</p>`+
     //                   `<p class="card-text">Total Meninggal : ${res.meninggal}</p>`+
     //                   `<p class="card-text">Total Sembuh : ${res.sembuh}</p>`+
