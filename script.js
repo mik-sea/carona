ed99a7a5c77ee80a84a4c6e35871457a40fab99a
Glitch (hello-webpage) <none>
2020-03-18 16:13:37 +0000

    🚩🍇 Checkpoint ./search.js:12720927/64:12940608/407 ./index.html:12720927/9:12942465/896 ./script.js:12720927/50 ./style.css:12942465/775

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
 let res = []
 //prepare for get api from api.kawalcorona.com
 var url = "https://api.kawalcorona.com"
--- search.js
+++ search.js
@@ -1,2 +1,18 @@
-var search = document.getElementById('search');
-var container =  document.getElementById('');
+var search = document.getElementsById('search');
+var container =  document.getElementsByClassName('container');
+
+search.addEventListener('keyup', function(){
+    
+    var ajax =  new XMLHttpRequest();
+  
+    ajax.onreadystatechange = function(){
+  
+      if(ajax.readyState == 4 && ajax.status == 200){
+          container.innerHTML == ajax.responseText;
+      }
+    
+    }
+  
+      ajax.open('GET', 'output.html?search=' )
+  
+});
