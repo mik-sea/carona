ce3efdb66f0fe45f75d191a711531c8e2e022b93
Glitch (carona) <none>
2020-03-19 15:19:25 +0000

    💖👠 Checkpoint ./index.html:12720927/92:12940608/1231 ./search.js:12940608/433 ./script.js:12720927/459

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 let res = {}
 //prepare for get api from api.kawalcorona.com
-var url = "https://api.kawalcorona.com"
-function getDetail(hasil){
-  // console.log(data)
-    hasil.forEach(response=>{
-        let dataa = response.attributes
-        // console.log(dataa)
-        res.negara = dataa.Country_Region
-        res.lat = dataa.Lat
-        res.long = dataa.Long_
-        res.terinfeksi = dataa.Confirmed
-        res.meninggal = dataa.Deaths
-        res.sembuh = dataa.Recovered
-        res.aktif = dataa.Active
-        // res.last_update = new Date(1584469982000/1000)
-        // console.log(res.last_update)
-        $("#negara").append(`<a class="dropdown-item" href="#">${res.negara}</a>`)
-        $("div#dataa").append(`<div class="col-lg-6 pb-5">`+
-            `<div class="card text-center">`+
-            `<div class="card-header" id="nama-negara">`+
-            `<h5 class="card-title">${res.negara}</h5>`+
-            `</div>`+
-            `<div class="card-body" id="hasil">`+
-            `<p class="card-text">Total Terinfeksi : ${res.terinfeksi}</p>`+
-            `<p class="card-text">Total Meninggal : ${res.meninggal}</p>`+
-            `<p class="card-text">Total Sembuh : ${res.sembuh}</p>`+
-            `<p class="card-text">Total Aktif : ${res.aktif}</p>`+
-            `</div>`+
-            `</div>`+
-        `</div>`)
-    })
-}
+var url = "https://api.kawalcorona.com/indonesia"
+// function getDetail(hasil){
+//   console.log(hasil)
+//     hasil.forEach(response=>{
+//         // let dataa = response.attributes
+//         let dt = response
+//         // console.log(dataa)
+//         // res.negara = dataa.Country_Region
+//         // res.lat = dataa.Lat
+//         // res.long = dataa.Long_
+//         // res.terinfeksi = dataa.Confirmed
+//         // res.meninggal = dataa.Deaths
+//         // res.sembuh = dataa.Recovered
+//         // res.aktif = dataa.Active
+//         // res.last_update = new Date(1584469982000/1000)
+//         // console.log(res.last_update)
+//         $("#negara").append(`<a class="dropdown-item" href="#">${res.negara}</a>`)
+//         $("div#dataa").append(`<div class="col-lg-6 pb-5">`+
+//             `<div class="card text-center">`+
+//             `<div class="card-header" id="nama-negara">`+
+//             `<h5 class="card-title">${dt.name}</h5>`+
+//             `</div>`+
+//             `<div class="card-body" id="hasil">`+
+//             `<p class="card-text">Total Terinfeksi : ${dt.positif}</p>`+
+//             `<p class="card-text">Total Meninggal : ${dt.meninggal}</p>`+
+//             `<p class="card-text">Total Sembuh : ${dt.sembuh}</p>`+
+//             // `<p class="card-text">Total Aktif : ${dt.aktif}</p>`+
+//             `<p class="card-text">Last Update : ${dt.lastupdate}</p>`
+//             `</div>`+
+//             `</div>`+
+//         `</div>`)
+//     })
+// }
 
-fetch(url).then(response=>{
-    response.json().then(hasil=>{
-        getDetail(hasil)
+// fetch(url).then(response=>{
+//     response.json().then(hasil=>{
+//         getDetail(hasil)
+//     })
+// })
+    $.ajax({
+        url:url,
+        type:"GET",
+        headers:{
+            "content-type":"application/json",
+            // "Access-Control-Allow-Origin":"*",
+        },
+        success:(res)=>{
+            console.log(res)
+        }
     })
-})
-    // $.ajax({
-    //     url:url,
-    //     type:"GET",
-    //     headers:{
-    //         // "content-type":"application/json",
-    //         "Access-Control-Allow-Origin":"*",
-    //     },
-    //     success:(res)=>{
-    //         console.log(res)
-    //     }
-    // })
--- search.js
+++ search.js
@@ -1,8 +1,20 @@
 var search = document.getElementById('search');
 var container =  document.getElementsByClassName('');
 
-search.addEventListener('onclick', function(){
+search.addEventListener('keyup', function(){
     
+    var ajax =  new XMLHttpRequest();
+  
+    ajax.onreadystatechange = function(){
+  
+      if(ajax.readyState == 4 && ajax.status == 200){
+          container.innerHTML = ajax.responseText;
+      }
     
+    }
+  
+      ajax.open('GET', 'output.html?search=' + search.value, true);
+      ajax.send(); 
+      
   
 }   
