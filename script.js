2cf9006a1a548957d32e3a688e8b0b5719c80607
Glitch (carona) <none>
2020-03-20 16:17:20 +0000

    🏣🍳 Checkpoint ./script.js:12720927/1123:12942465/67 ./index.html:12720927/42:12942465/824

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ let res = {}
 //prepare for get api from api.kawalcorona.com
 var url = "https://api.kawalcorona.com"
 $(document).ready(function(){
-    function cekNegara(){
-        console.log($("div#nama-negara"))
-    }
-    // cekNegara()
+    // $('.card').each(function(){
+    //     $(this).attr('searchData', $(this).text().toLowerCase());
+    // });
+    // $('#boxSearch').on('keyup', function(){
+    // var dataList = $(this).val().toLowerCase();
+    //     $('.card').each(function(){
+    //         if ($(this).filter('[searchData *= ' + dataList + ']').length > 0 || dataList.length < 1) {
+    //             $(this).show();
+    //         } else {
+    //             $(this).hide();
+    //         }
+    //     });
+    // });
 })
 function getUrlVars(param=null){
     if(param !== null)
@@ -42,23 +51,25 @@ function getDetail(hasil){
             res.waktu = `${waktuu.toLocaleDateString()}&nbsp;${waktuu.toLocaleTimeString(undefined,{hour12:false})}`
             $("#negara").append(`<a class="dropdown-item" href="luar-negeri.html?negara=${res.negara}">${res.negara}</a>`)
             $("div#dataa").append(
-                `<ul><li><div class="col-lg-6 pb-5">`+
+                `<div class="col-lg-6 pb-5">`+
                 `<div class="card text-center bg-light">`+
                 `<div class="card-header bg-dark" id="nama-negara" data-theme="dark">`+
                 `<h5 class="card-title">${res.negara}</h5>`+
                 `</div>`+
                 `<div class="card-body" id="hasil" data-theme="dark">`+
-                `<p class="card-text">Total Terinfeksi : ${res.positif} Jiwa</p>`+
-                `<p class="card-text">Total Meninggal : ${res.meninggal} Jiwa</p>`+
-                `<p class="card-text">Total Sembuh : ${res.sembuh} Jiwa</p>`+
-                `<p class="card-text">Total Aktif : ${res.aktif} Jiwa</p>`+
+                `<p class="card-text">Total Terinfeksi : ${res.positif} Jiwa😱</p>`+
+                `<p class="card-text">Total Meninggal : ${res.meninggal} Jiwa👻</p>`+
+                `<p class="card-text">Total Sembuh : ${res.sembuh} Jiwa😃</p>`+
+                `<p class="card-text">Total Aktif : ${res.aktif} Jiwa😷</p>`+
                 `<p class="card-text">Last Update : ${res.waktu}</p>`+
                 `</div>`+
                 `</div>`+
-            `</div></li></ul>`)
-            cetak(res.negara)
+            `</div>`
+            )
+        // cetak(res.negara)
         })
     }
+
 fetch(url).then(response=>{
     response.json().then(hasil=>{
         getDetail(hasil)
