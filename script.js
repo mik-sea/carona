7aea4f3fe7a5b61eeae35b6beff5e0c80478606f
Glitch (carona) <none>
2020-03-20 17:22:47 +0000

    🏛🍀 Checkpoint ./script.js:12942465/85 ./dark-mode-switch.js:12942465/8

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
         });
     });
 })
+
 function getUrlVars(param = null){
     if(param !== null)
     {
@@ -41,6 +42,7 @@ function getUrlVars(param = null){
 function getDetail(hasil){
     //   console.log(hasil)
         hasil.forEach(response=>{
+          
             let dataa = response.attributes
             // let dt = response
             // console.log(dataa)
@@ -51,6 +53,7 @@ function getDetail(hasil){
             res.meninggal = dataa.Deaths
             res.sembuh = dataa.Recovered
             res.aktif = dataa.Active
+          
             let waktuu = new Date(dataa.Last_Update)
             res.waktu = `${waktuu.toLocaleDateString()}&nbsp;${waktuu.toLocaleTimeString(undefined,{hour12:false})}`
             $("#negara").append(`<a class="dropdown-item" href="luar-negeri.html?negara=${res.negara}">${res.negara}</a>`)
@@ -85,9 +88,9 @@ function cetak(data) {
     localStorage.setItem('search', JSON.stringify(search1.toLowerCase()))
     let get = localStorage.getItem('search')
     if(data.toLowerCase() == get){
-        // console.log("tidak")
+        console.log("tidak")
     }else{
-        // console.log(data)
+        console.log(data)
     }
     // const data = JSON.parse(localStorage.getItem('search'))
     // console.log(localStorage.getItem('search'))
